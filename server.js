a308f524475adfef89c32056f53ae3e8f726ae20
harisan17 <junior_hari@yahoo.co.uk>
2019-10-23 22:02:45 -0400

    major changes1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const request = require("request");
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -11,12 +12,91 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res) {
-  var firstNumber = parseInt(req.body.firstNumber);
-  var secondNumber = parseInt(req.body.secondNumber);
-  var result = firstNumber + secondNumber;
-  res.send("Sum is:" + result);
+app.get("/api1", function(req, res) {
+  res.sendFile(__dirname + "/testapi1.html")
 });
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// POST
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+app.post("/", function(req, res) {
+
+  // console.log(req.body);
+  // console.log(req.body.firstNumber);
+  if (req.body.submit == "SumNumbers") {
+
+    var firstNumber = parseInt(req.body.firstNumber);
+    var secondNumber = parseInt(req.body.secondNumber);
+    var result = firstNumber + secondNumber;
+    res.send("Sum is:" + result);
+
+  } else {
+
+    // First Request --- get location details & key of the city
+    //** note: requires the city name to be passed under the query string qs. qs - keyword
+    var url = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    var options = {
+      url: url,
+      method: "GET",
+      qs: {
+        apikey: "cy4dHXTMiuXBG7Nblr0EDZH7pgBcH7Er",
+        q: req.body.location
+      }
+    };
+
+    var jsonData = "";
+    var jsonData2 = "";
+
+
+    request(options, function(error, response, body) {
+      jsonData = JSON.parse(body);
+
+      // Second Request - get weather details for key
+      //** note: this requires the key to be mentioned in the URL as per the API reference Info.
+      var url1 = ("http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/" + jsonData[0].Key);
+      var options1 = {
+        url: url1,
+        method: "GET",
+        qs: {
+          apikey: "cy4dHXTMiuXBG7Nblr0EDZH7pgBcH7Er"
+        }
+      };
+      request(options1, function(error, response, body) {
+        jsonData2 = JSON.parse(body);
+
+        res.write("<html>" + "<body> <ul>");
+        res.write("<li>" + jsonData[0].EnglishName + "</li>");
+        res.write("<li>" + jsonData[0].Region.LocalizedName + "</li>");
+        res.write("<li>" + jsonData[0].AdministrativeArea.LocalizedName + "</li>");
+        res.write("<li>" + jsonData[0].GeoPosition.Latitude + "," + jsonData[0].GeoPosition.Longitude + "</li>");
+        res.write("<li>" + jsonData[0].TimeZone.Code + "</li>");
+
+        // res.write("<html>" + "<body> <ul>");
+        var tempFar = parseFloat(jsonData2[0].Temperature.Value);
+        var tempCel = Math.round((tempFar - 32) / 1.8);
+        res.write("<li>" + jsonData2[0].IconPhrase + "</li>");
+        res.write("<li> Rains: " + jsonData2[0].HasPrecipitation + "</li>");
+        res.write("<li>" + tempCel + "C" + "</li>");
+        // jsonData2[0].Temperature.Unit
+        res.write("</ul> </body> </html>");
+
+        res.send();
+      });
+
+
+    });
+
+
+
+  }
+
+});
+
+
+
+
+// cy4dHXTMiuXBG7Nblr0EDZH7pgBcH7Er
+// http://dataservice.accuweather.com/locations/v1/cities/search?apiKey="cy4dHXTMiuXBG7Nblr0EDZH7pgBcH7Er"&q="Toronto"
 
 
 
